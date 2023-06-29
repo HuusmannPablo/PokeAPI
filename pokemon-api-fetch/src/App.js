ce418d96e766ed5cd772e3201153b2d60b432e4f
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-06-29 20:33:47 +1200

    fetching component working, duplicate key issue to solve

--- pokemon-api-fetch/src/App.js
+++ pokemon-api-fetch/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import FetchingComponent from './fetchingComponent';
 
 function App() {
   return (
     <div className="App">
       <header>Pokemon API fetching application</header>
+      <FetchingComponent />
     </div>
   );
 }
---
+++ pokemon-api-fetch/src/fetchingComponent.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+
+function FetchingComponent() {
+    
+    const [pokemonData, setPokemonData] = useState([]);
+    
+    useEffect(() => {
+        // fetchPokemonData();
+        fetchKantoPokemon();
+    }, []);
+
+    // const fetchPokemonData = async () => {
+    //     try {
+    //         const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+    //         const data = await response.json();
+    //         console.log(data); 
+    //     } 
+    //     catch (error) {
+    //         console.log('Error fetching data:', error);
+    //     }
+    // };
+
+    function fetchKantoPokemon() {
+        fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+        .then(response => response.json())
+        .then(function(allpokemon) {
+            allpokemon.results.forEach(function(pokemon){
+                fetchPokemonData(pokemon);
+            });
+        });
+    };
+    
+    function fetchPokemonData(pokemon) {
+        let url = pokemon.url
+    
+        fetch(url)
+        .then((response) => response.json())
+        .then(function(pokeData) {
+            setPokemonData((prevData) => [...prevData, pokeData]);
+            // console.log(pokeData);
+        });
+    };
+
+    return (
+        <div>
+            <h1>Fetched Data</h1>
+            {pokemonData.map((item) => (     
+                <div key={item.id}>
+                    <p>{item.name}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default FetchingComponent;
