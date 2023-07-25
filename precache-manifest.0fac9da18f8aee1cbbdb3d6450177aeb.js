96d33f2cbb0494f3fbf682803f218330c7270d17
Pablo Huusmann <105261635+HuusmannPablo@users.noreply.github.com>
2023-07-25 10:29:57 -0300

    Updates

---
+++ precache-manifest.0fac9da18f8aee1cbbdb3d6450177aeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a369c0e2ec5b67bac21",
+    "url": "/huusmannpablo.github.io/PokeAPI/static/css/main.c5fa22d1.chunk.css"
+  },
+  {
+    "revision": "9a369c0e2ec5b67bac21",
+    "url": "/huusmannpablo.github.io/PokeAPI/static/js/main.9a369c0e.chunk.js"
+  },
+  {
+    "revision": "23e5026d3c124a934bc7",
+    "url": "/huusmannpablo.github.io/PokeAPI/static/js/1.23e5026d.chunk.js"
+  },
+  {
+    "revision": "ba5817d8b3349e44ae99",
+    "url": "/huusmannpablo.github.io/PokeAPI/static/js/2.ba5817d8.chunk.js"
+  },
+  {
+    "revision": "d4ace60e7090d914b6e7",
+    "url": "/huusmannpablo.github.io/PokeAPI/static/js/runtime~main.d4ace60e.js"
+  },
+  {
+    "revision": "0eb6fbff7c75c3adcb4c650411c37aa3",
+    "url": "/huusmannpablo.github.io/PokeAPI/index.html"
+  }
+];
